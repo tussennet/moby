353d49a1db09f1e1042fca5742c5a378ad36780b
tussennet <test@test.nl>
2020-09-04 15:07:43 +0200

    merged type files

--- daemon/monitor_freebsd.go
+++ daemon/monitor_freebsd.go
@@ -2,17 +2,17 @@ package daemon
 
 import (
 	"github.com/docker/docker/container"
-	"github.com/docker/docker/libcontainerd"
+	"github.com/docker/docker/libcontainerd/types"
 )
 
 // platformConstructExitStatus returns a platform specific exit status structure
-func platformConstructExitStatus(e libcontainerd.StateInfo) *container.ExitStatus {
+func platformConstructExitStatus(e types.StateInfo) *container.ExitStatus {
 	return &container.ExitStatus{
 		ExitCode: int(e.ExitCode),
 	}
 }
 
 // postRunProcessing perfoms any processing needed on the container after it has stopped.
-func (daemon *Daemon) postRunProcessing(container *container.Container, e libcontainerd.StateInfo) error {
+func (daemon *Daemon) postRunProcessing(container *container.Container, e types.StateInfo) error {
 	return nil
 }
